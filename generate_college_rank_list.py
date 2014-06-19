1cad9d7d1885338a5b4f8f5f9174e5653d98d779
achiever202 <es12b1001@iith.ac.in>
2014-06-19 21:45:13 +0530

    This function generates the college rank list from the entire rank list.

---
+++ generate_college_rank_list.py
@@ -0,0 +1,17 @@
+import sys
+import urllib2
+from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
+import check_user
+
+def generate_college_rank_list(college):
+	read_file = open("try.txt", "r");
+	write_file = open("college_rank_list.txt", "w")
+
+	for line in read_file:
+		user_info = line.split(" ");
+		user_name = check_user.check_user(user_info[1], college)
+		if(user_name!=None):
+			write_file.write(user_info[0] + " " + user_name + " " + user_info[2] + "\n")
+		
+
