acc98bbb85616f4efb6b6f58d8dceee66027629d
achiever202 <es12b1001@iith.ac.in>
2014-06-19 17:01:30 +0530

    Created Main python file for the college rank list generator.

---
+++ codechef_college_rank_generator.py
@@ -0,0 +1,14 @@
+import sys
+import urllib2
+from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
+import generate_rank_list	
+
+arguments = sys.argv[1:]
+
+if len(arguments)>2:
+	print "Too many arguments for the college rank list generator."
+else:
+	generate_rank_list.generate_rank_list(arguments)
+		
+
